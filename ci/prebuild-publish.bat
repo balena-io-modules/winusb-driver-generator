64a857a59e4911a94af3f2e1c2f0e4e107638806
Juan Cruz Viotti <jv@jviotti.com>
2017-12-08 11:00:21 -0400

    Fix prebuild script (again)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- ci/prebuild-publish.bat
+++ ci/prebuild-publish.bat
@@ -2,6 +2,7 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    npm run upload-prebuilds -- -u %GITHUB_TOKEN%
+    call node_modules\.bin\prebuild --runtime electron --target 1.7.9 --strip -u %GITHUB_TOKEN%
+    call node_modules\.bin\prebuild --runtime node --target 6.1.0 --strip -u %GITHUB_TOKEN%
   )
 )
