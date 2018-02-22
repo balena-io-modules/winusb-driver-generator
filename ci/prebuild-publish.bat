a1a3b669034e3b230bed3ce39eba707c7636d806
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-02-22 14:27:35 +0100

    chore(package): Prebuild for all targets & runtimes
    This enables prebuilding for all Node & Electron target ABIs
    
    Change-Type: patch

--- ci/prebuild-publish.bat
+++ ci/prebuild-publish.bat
@@ -2,7 +2,6 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    call node_modules\.bin\prebuild --runtime electron --target 1.7.9 --strip -u %GITHUB_TOKEN%
-    call node_modules\.bin\prebuild --runtime node --target 6.1.0 --strip -u %GITHUB_TOKEN%
+    call node_modules\.bin\prebuild --all --strip -u %GITHUB_TOKEN%
   )
 )
