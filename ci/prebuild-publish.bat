7195da22143ecfad7b22419efd24366c0959e8f7
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-04-02 21:31:12 +0200

    chore(ci): Add prebuild targets for new Node/Electron versions
    Change-Type: patch

--- ci/prebuild-publish.bat
+++ ci/prebuild-publish.bat
@@ -2,7 +2,7 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    call node_modules\.bin\prebuild --strip --runtime electron --target 1.7.11 -u %GITHUB_TOKEN%
-    call node_modules\.bin\prebuild --strip --runtime node --target 8.10.0 --target 6.13.1 -u %GITHUB_TOKEN%
+    call node_modules\.bin\prebuild --strip --runtime electron --target 1.7.13 --target 1.8.7 --target 2.0.0 -u %GITHUB_TOKEN%
+    call node_modules\.bin\prebuild --strip --runtime node --target 6.13.1 --target 8.10.0 --target 10.0.0 -u %GITHUB_TOKEN%
   )
 )
